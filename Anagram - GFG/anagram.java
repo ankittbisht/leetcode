baeb47bf1934a2d21c442d34e2f0f2231334cdea
ankittbisht <110447589+ankittbisht@users.noreply.github.com>
2023-03-18 11:29:39 +0530

    Added solution - LeetHub

---
+++ Anagram - GFG/anagram.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args)throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		while(t-->0)
+		{
+		    String str = br.readLine();
+		    String s1 = str.split(" ")[0];
+		    String s2 = str.split(" ")[1];
+		    
+		    Solution obj = new Solution();
+		    
+		    if(obj.isAnagram(s1,s2))
+		    {
+		        System.out.println("YES");
+		    }
+		    else
+		    {
+		         System.out.println("NO");
+		    }
+		    
+		    
+		    
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{    
+    //Function is to check whether two strings are anagram of each other or not.
+    public static boolean isAnagram(String a,String b)
+    {
+        
+        // Your code here
+        char first[] = a.toCharArray();
+        char second[] = b.toCharArray();
+        Arrays.sort(first);
+        Arrays.sort(second);
+        if(first.length != second.length){
+            return false;
+        }
+        for(int i = 0 ; i < first.length ;i++){
+            if(first[i]!= second[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
