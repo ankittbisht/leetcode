52bcdddff7273e639ec56f50266bde81d79e9d36
ankittbisht <110447589+ankittbisht@users.noreply.github.com>
2023-03-18 12:37:12 +0530

    Added solution - LeetHub

---
+++ Remove Duplicates - GFG/remove-duplicates.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s = read.readLine();
+            
+            Solution ob = new Solution();
+            String result = ob.removeDups(s);
+            
+            System.out.println(result);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    String removeDups(String S) {
+        // code here
+        
+        String str = new String();
+        int len = S.length();
+         
+        // loop to traverse the string and
+        // check for repeating chars using
+        // IndexOf() method in Java
+        for (int i = 0; i < len; i++)
+        {
+            // character at i'th index of s
+            char c = S.charAt(i);
+             
+            // if c is present in str, it returns
+            // the index of c, else it returns -1
+            if (str.indexOf(c) < 0)
+            {
+                // adding c to str if -1 is returned
+                str += c;
+            }
+        }
+         
+        return str;
+        
+    }
+}
