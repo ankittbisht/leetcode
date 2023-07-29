c18565ec96b8c9e058cbf12f47389a115ffa58bf
Ankit Bisht <110447589+ankittbisht@users.noreply.github.com>
2023-07-29 13:13:03 +0530

    Added solution - LeetHub

---
+++ Segregate even and odd nodes in a Link List - GFG/segregate-even-and-odd-nodes-in-a-link-list.java
@@ -0,0 +1,125 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+class FastReader{ 
+    BufferedReader br; 
+    StringTokenizer st; 
+
+    public FastReader(){ 
+        br = new BufferedReader(new InputStreamReader(System.in)); 
+    } 
+
+    String next(){ 
+        while (st == null || !st.hasMoreElements()){ 
+            try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    String nextLine(){ 
+        String str = ""; 
+        try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+        return str; 
+    } 
+    
+    Integer nextInt(){
+        return Integer.parseInt(next());
+    }
+} 
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+class GFG{
+	static void printList(Node node, PrintWriter out) 
+	{ 
+		while (node != null) 
+		{ 
+			out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            Solution ob = new Solution();
+            Node ans = ob.divide(n, head);
+            printList(ans, out); 
+            t--;
+        }
+        out.flush();
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* 
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution{
+    Node divide(int N, Node head){
+        Node evenStart = null;
+        Node evenEnd = null;
+        Node oddStart = null;
+        Node oddEnd = null;
+        Node curr = head;
+        while(curr != null){
+            int val = curr.data;
+            if(val%2==0){
+                if(evenStart==null){
+                    evenStart = curr;
+                    evenEnd = evenStart;
+                }else{
+                    evenEnd.next = curr;
+                    evenEnd = evenEnd.next;
+                }
+            }else{
+                if(oddStart==null){
+                    oddStart = curr;
+                    oddEnd = oddStart;
+                }else{
+                    oddEnd.next = curr;
+                    oddEnd = oddEnd.next;
+                }
+            }
+        curr = curr.next;
+        }
+        if(oddStart == null || evenStart == null) {
+            return head;
+        }
+        evenEnd.next = oddStart;
+        oddEnd.next = null;
+        return evenStart;
+    }
+}
